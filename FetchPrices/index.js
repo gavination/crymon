693301f98b1f10536437f197ecbda57655eaa8a9
rdc64 <chrobles@microsoft.com>
2017-08-30 12:13:42 -0400

    working on table storage

--- FetchPrices/index.js
+++ FetchPrices/index.js
@@ -1,21 +1,40 @@
-const request = require('request');
+const request = require('request')
+tracked = ['BTC', 'ETH', 'GNT', 'REP']
 
-module.exports = function (context, tickerTimer) {
-    var timeStamp = new Date().toISOString();
+module.exports = function (context) {
+
+    request('https://api.coinmarketcap.com/v1/ticker/', function (error, response, body) {
+        
+        if (error || response.statusCode != 200) {
+            context.log('error:', error)
+            context.log('statusCOde:', response && response.statusCode)
+        }
+        
+        var tickerData = JSON.parse(body)
+        var coinData = []
+
+        for (var coin in tickerData) {
+            if (tracked.indexOf(tickerData[coin]["symbol"]) > -1) {
+                context.log(tickerData[coin]["symbol"], "=>", tickerData[coin]["percent_change_1h"])
+                coinData.push(tickerData[coin])
+            }
+        }
+
+        var coinInfo = {
+            "timestamp": new Date().toISOString(),
+            "data": coinData
+        }
+
+        context.bindings.marketStatsTable = []
+        context.bindings.marketStatsTable.push({
+            PartitionKey:"MarketData1",
+            RowKey: new Date().toISOString(),
+            Data: coinData
+        })
+
+        context.log(coinInfo)
+        context.done()
     
-    if(tickerTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    })
 
-    request('https://api.coinmarketcap.com/v1/ticker/bitcoin/', function (error, response, body) {
-        context.log('error:', error);
-        context.log('statusCode:', response && response.statusCode);
-        context.log('body:', body);
-        context.log(body[0])
-    });
-
-    context.done();
-};
-
+}
